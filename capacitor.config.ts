e75a7e8778975b3a437bceb063c417e5e625aaea
assimfaloudanield-lang <assimfaloudanield@gmail.com>
2026-09-06 12:53:59 +0000

    chore: setup capacitor for android build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.lyx',
+  appName: 'LYX',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
